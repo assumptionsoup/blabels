eedcebd8f59fe584dadcec87c5f3c33c3de45c87
Jordan Hueckstaedt <assumptionsoup@gmail.com>
2012-08-11 19:49:01 -0700

    Added Lisence and Readme

--- advanced_shape_key_panel.py
+++ advanced_shape_key_panel.py
@@ -1,3 +1,26 @@
+''' Replaces the default shape key panel with an "Advanced" one.
+
+Adds the ability to sort shape keys by labels, in addition to a number
+of other shape key related operations.'''
+
+'''
+*******************************************************************************
+	License and Copyright
+	Copyright 2012 Jordan Hueckstaedt
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
 import bpy
 from mathutils import Vector
 from bpy.types import Menu, Panel
